e250b0cd4a8dbbf42d615e8983eecff4ce0b5cf5
zachary <zachary.rowden@showmax.com>
2024-02-09 00:36:06 +0100

    Hello world, basic working processing sketch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 import processing.core.PApplet;
-public class Main {
+
+public class Main extends PApplet {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		PApplet.main("Main"); // Use the string "Main" to refer to this class
+	}
+
+	public void settings() {
+		size(640, 480); // Set the size of the window
+	}
+
+	public void setup() {
+		background(255); // Set the background to white
+	}
+
+	public void draw() {
+		ellipse(width / 2, height / 2, 100, 100); // Draw a circle in the middle of the window
 	}
 }
